2bf97c35accc066e1e290ba94323988f0bbd8e6d
rverma-nikiai <rohit.verma@niki.ai>
2019-06-23 07:20:52 +0530

    pinned null label

--- private.tf
+++ private.tf
@@ -1,5 +1,5 @@
 module "private_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.13.0"
   context    = module.label.context
   attributes = compact(concat(module.label.attributes, ["private"]))
 
